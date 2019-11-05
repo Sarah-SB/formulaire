f3527772b70366b7ff85ee730f6e75cfb0d524ac
sarah-sb <sarah.sbini@gmail.com>
2019-11-05 16:52:23 +0100

    Mise a jour de la class Utilisateur

--- src/css/Utilisateur.java
+++ src/css/Utilisateur.java
@@ -1,4 +1,35 @@
 package css;
 
 public class Utilisateur {
+
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private int codePostal;
+    private String ville;
+    private String email;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
